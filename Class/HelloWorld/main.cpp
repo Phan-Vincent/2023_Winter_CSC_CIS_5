d7b3facbfafe1ed07aacb23c9368cbe0751875b3
Vincent Phan <phanvincent2@gmail.com>
2023-01-05 16:40:50 -0800

    Signed-off-by: Vincent Phan <phanvincent2@gmail.com>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Vincent Phan
+ * Created on January 5, 2023, 4:05 PM
+ * Purpose: Template to be used for all 
+ * future Hmwk, Labs, Exams, Projects
+ */
+
+//System Libraries
+#include <iostream> // I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World"<<endl;
+    //Clean up memory and files
+    
+    //Exit the program
+    
+    
+    return 0;
+}
+
